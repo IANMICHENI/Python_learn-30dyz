9be8df943272f73d6e444f0da6b2efa9f6bd6cef
Ian Mwenda <ian.micheni@student.moringaschool.com>
2025-11-24 09:22:48 +0300

    print

---
+++ Day01/Intro.py
@@ -0,0 +1,9 @@
+#python program that prints your name, age,country on seperate lines
+name = "Ian"
+age = 21
+country = "Kenya"
+
+print("====Print====")
+print(f"Your name is: {name}")
+print(f"You are {age} old")
+print(f"You live in {country}")
