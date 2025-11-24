5c6f54572e982c549a752c5a208bc4f06f6bb2b3
Ian Mwenda <ian.micheni@student.moringaschool.com>
2025-11-24 09:48:16 +0300

    Art

---
+++ Day01.exercises/10prints.py
@@ -0,0 +1,3 @@
+#Script that prints "Python is awesome 10 times"
+for i in range(10):
+    print("Python is awesome!")
---
+++ Day01.exercises/AskUserInput.py
@@ -0,0 +1,3 @@
+#Script that asks user for their name, and greets them using a personalized message
+name = input("Enter Your Name: ")
+print(f"Hello {name}, How are you?")
