79d88cebbe5b18379a197c49dcc6bf4d88ab76d8
Ian Mwenda <ian.micheni@student.moringaschool.com>
2025-11-25 10:33:21 +0300

    Vars

---
+++ Day03.exercises/Variables.py
@@ -0,0 +1,10 @@
+''' Create a variable for your age (integer)
+
+Create a variable for your height (float)
+
+Create a variable that stores a complex number  '''
+
+int_age = 21
+float_height = 1.75
+var_num = 2 + 5j
+var_num = complex(2,5)
